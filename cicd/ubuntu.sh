eb4d27f32703afc02f09736eb2bc65ebec59d178
Vladyslav Rehan <rehanvladyslav@gmail.com>
2025-04-30 21:43:27 +0300

    Try to fix cd

--- cicd/ubuntu.sh
+++ cicd/ubuntu.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env sh 
 
+show () {
+	echo "ubuntu.sh: $1"
+}
+
 # installing dependencies
+show "Installing texlive"
 sudo apt install texlive
+show "Installing latexmk"
 sudo tlmgr install latexmk
 
 SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
